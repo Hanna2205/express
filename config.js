495b6828cf46dc056a59b7710bcd41ed28997493
Arek Sz <arek@wp.pl>
2020-05-26 11:01:03 +0200

    config db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: '',
+  db: 'mongodb+srv://kazik865admin:HOH0PcD986MsBUDM@cluster0-wvvvr.mongodb.net/test?retryWrites=true&w=majority',
   keySession: ['TWOJKLUCZ'],
   maxAgeSession: 24 * 60 * 60 * 1000
 }
